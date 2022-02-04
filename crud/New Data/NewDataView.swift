b16a2b0713e7acd33a1226f5ddb71dc9ddcf3aee
erickmxav <erickmxav@gmail.com>
2022-02-03 23:21:31 -0300

    NewData
    screen that add some new task in the app

---
+++ crud/New Data/NewDataView.swift
@@ -0,0 +1,73 @@
+//
+//  NewDataView.swift
+//  crud
+//
+//  Created by Erick Martins on 02/02/22.
+//
+
+import SwiftUI
+import CoreData
+
+struct NewDataView: View {
+    
+    @ObservedObject var homeData: HomeViewModel
+    @Environment(\.managedObjectContext) var context
+    var body: some View {
+        
+        VStack{
+            HStack{
+                
+                Text("\(homeData.updateItem == nil ? "Adicione Uma" : "Update") Tarefa")
+                    .font(.system(size: 60))
+                    .fontWeight(.heavy)
+                    .foregroundColor(.black)
+                
+                Spacer(minLength: 0)
+            }
+            .padding()
+            
+            TextEditor(text: $homeData.content)
+                .padding()
+            
+            Divider()
+                .padding(.horizontal)
+            
+            HStack{
+                Text("Clique Abaixo Para Confirmar")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(.black)
+                
+                Spacer(minLength: 0)
+            }
+            .padding()
+            
+            Button(action: {homeData.writeData(context: context)}, label: {
+                
+                Label(
+                    title: { Text(homeData.updateItem == nil ? "Adicionar" : "Editar")
+                        .font(.title2)
+                        .foregroundColor(.white)
+                        .fontWeight(.bold)
+                    },
+                    icon: { Image(systemName: "plus")
+                        .font(.title2)
+                        .foregroundColor(.white)
+                    })
+                    .padding(.vertical)
+                    .frame(width: UIScreen.main.bounds.width - 30)
+                    .background(
+                        
+                        LinearGradient(gradient: .init(colors: [Color("Color"), Color("Color1")]), startPoint: .leading, endPoint: .trailing)
+                    )
+                    .cornerRadius(8)
+            })
+            .padding()
+            .disabled(homeData.content == "" ? true : false)
+            .opacity(homeData.content == "" ? 0.5 : 1)
+        }
+        
+        .background(Color.black.opacity(0.06).ignoresSafeArea(.all, edges: .bottom))
+    }
+}
+
